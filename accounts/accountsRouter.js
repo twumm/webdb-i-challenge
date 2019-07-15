b67bfa4db3587922429c60fcf76d1052ff0bc7a8
Martin Twum Mensah <martint.mensah@gmail.com>
2019-07-15 13:16:12 +0000

    feature: Create method to get all accounts from the db

--- accounts/accountsRouter.js
+++ accounts/accountsRouter.js
@@ -3,4 +3,14 @@ const express = require('express');
 const router = express.Router();
 const accountsDb = require('./accountsDb');
 
+router.get('/', async (req, res, next) => {
+  try {
+    const accounts = await accountsDb.get();
+    res.status(200).json(accounts);
+  }
+  catch (error) {
+    new Error('Could not get accounts. Kindly try again');
+  }
+});
+
 module.exports = router;
