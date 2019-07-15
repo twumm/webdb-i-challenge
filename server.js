0c51e6a5f73438098902af82a08eb4ab1f799b8b
Martin Twum Mensah <martint.mensah@gmail.com>
2019-07-15 12:54:28 +0000

    feature: Send html for main path

--- server.js
+++ server.js
@@ -8,4 +8,8 @@ const server = express();
 server.use(express.json());
 server.use('/api/accounts', accountsRouter);
 
+server.get('/', (req, res, next) => {
+  res.status(200).send('<h2>You to account api</h2>')
+})
+
 module.exports = server;
