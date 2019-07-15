de988a6b46b65995ce22e9d5fe5c5d8a9938f983
Martin Twum Mensah <martint.mensah@gmail.com>
2019-07-15 13:34:05 +0000

    refactor: Use if...else to resolve catch error

--- middlewares/accountsMiddleware.js
+++ middlewares/accountsMiddleware.js
@@ -3,14 +3,15 @@ const accountsDb = require('../accounts/accountsDb');
 async function validateAccountId(req, res, next) {
   const { id } = req.params;
   if (isNaN(Number(id))) {
-    res.status(400).json({ message: 'Account id must be a number' })
-  }
-  const account = await accountsDb.getById(id);
-  if (account) {
-    req.account = account;
-    next();
+    res.status(400).json({ message: 'Account id must be a number' }).end();
   } else {
-    res.status(400).json({ message: 'Invalid account id' });
+    const account = await accountsDb.getById(id);
+    if (account) {
+      req.account = account;
+      next();
+    } else {
+      res.status(400).json({ message: 'Invalid account id' });
+    }
   }
 }
 
