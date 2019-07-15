3ecbb665769fe3b6cf880b3c8e05452ffca8fe11
Martin Twum Mensah <martint.mensah@gmail.com>
2019-07-15 12:59:11 +0000

    feature: Add custom logger

--- server.js
+++ server.js
@@ -6,10 +6,18 @@ const accountsRouter = require('./accounts/accountsRouter');
 const server = express();
 
 server.use(express.json());
+server.use(logger);
 server.use('/api/accounts', accountsRouter);
 
 server.get('/', (req, res, next) => {
   res.status(200).send('<h2>You to account api</h2>')
 })
+
+function logger(req, res, next) {
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} request from ${req.url}`
+  );
+  next();
+}
 
 module.exports = server;
