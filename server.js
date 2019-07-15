8daadd3aefab1e42b8ba39071dd4026939d561d3
Martin Twum Mensah <martint.mensah@gmail.com>
2019-07-15 12:53:09 +0000

    feature: Import and use accountsRouter on path api-accounts

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
 const db = require('./data/dbConfig.js');
+const accountsRouter = require('./accounts/accountsRouter');
 
 const server = express();
 
 server.use(express.json());
+server.use('/api/accounts', accountsRouter);
 
 module.exports = server;
